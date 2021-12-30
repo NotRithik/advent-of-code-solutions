41d1b6a5318ff0303b6db73409adc2bbc9a89388
NotRithik <rithikum123@gmail.com>
2021-12-31 01:16:36 +0530

    Solve Day 2 problems

---
+++ 2021/Day 02/NotRithik_2.js
@@ -0,0 +1,59 @@
+// Run solution in NodeJS
+
+const fs = require('fs');
+
+let inputFileName;
+
+if ((process.argv).length > 2) {
+  args = process.argv.slice(2);
+  if (args[0] === '-f' || args[0] === '--filename') {
+    if (args[1].charAt(0) !== '-') {
+      inputFileName = args[1];
+    } else {
+      console.log('Input file specified incorrectly. Using Input_2.txt by default. Specify filename with --filename flag.');
+      process.exit(1);
+    }
+  } else {
+    inputFileName = 'Input_2.txt';
+    console.log('Input file not specified. Using Input_2.txt by default. Specify filename with --filename flag.');
+  }
+} else {
+  inputFileName = 'Input_2.txt';
+  console.log('Input file not specified. Using Input_2.txt by default. Specify filename with --filename flag.');
+}
+
+let inputData = fs.readFileSync(inputFileName, 'utf8', (err) => {
+  if (err) {
+    if (err.code === 'ENOENT') {
+      console.log('Input file not found.');
+      process.exit(1);
+    }
+    console.log(err);
+    process.exit(1);
+  }
+});
+
+let inputLines = inputData.split(/\r?\n/).map((inputLine) => {
+  let inputPair = inputLine.split(' ');
+  return [inputPair[0], parseInt(inputPair[1], 10)];
+});
+
+let depth = 0, horizontalPos = 0, aim = 0;
+
+for (let i = 0; i < inputLines.length; i++) {
+  inputLine = inputLines[i];
+  switch (inputLine[0]) {
+    case 'forward':
+      horizontalPos += inputLine[1];
+      depth += aim * inputLine[1];
+      break;
+    case 'down':
+      aim += inputLine[1];
+      break;
+    case 'up':
+      aim -= inputLine[1];
+      break;
+  }
+}
+
+console.log(depth * horizontalPos);
